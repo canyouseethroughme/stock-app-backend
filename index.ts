c54cd678fa98e1bb7c3ab72cbec688a36c2ecca4
Alin Chiosa <alin.c.wiredelta@gmail.com>
2022-06-21 11:39:32 +0200

    chore: added mongoose

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 
 const app = express()
 app.use(json())
+
+mongoose.connect('mongodb://localhost:27107/stockApp', {}, () =>
+    console.log('database connection successful')
+)
 
 app.listen(8080, () => console.log('App running on port 8080'))
