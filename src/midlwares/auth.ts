ceb676398e8234458e215ada99d52ef03c19a444
Alin Chiosa <alin.c.wiredelta@gmail.com>
2022-06-26 14:07:57 +0200

    chore: implemented authorization middleware

--- src/midlwares/auth.ts
+++ src/midlwares/auth.ts
@@ -1,29 +1,32 @@
+import { NextFunction, RequestHandler } from 'express'
 import jwt from 'jsonwebtoken'
 import { User } from '../models/user'
+import { TokenType } from '../types/types'
 
-const isAuthenticated = (req, res, next) => {
+export const isAuthenticated: RequestHandler = (req, res, next) => {
     const error = new Error('Unauthorized')
 
-    const token = req.headers.authorization
+    const token = req.get('Authorization')
+    console.log('🚀 ~ file: auth.ts ~ line 10 ~ token', token)
 
     if (!token || !token.length) {
         return next(error)
     }
 
     const data = jwt.verify(token.replace('Bearer ', ''), 'mysecretkey')
+
     if (!data) return next(error)
     // TODO: fix types
     User.findOne({
-        where: { id: data.userId },
+        where: { id: (data as TokenType).userId },
         attributes: { exclude: ['password'] },
     })
         .then((user) => {
-            req.user = user
+            // req.set.user = user
+            res.req.body.user = user
             return next()
         })
         .catch(() => {
             return next(error)
         })
 }
-
-module.exports = { isAuthenticated }
