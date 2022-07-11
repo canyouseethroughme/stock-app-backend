01805f0f367b3acff03e4024194a7b9811f5ab65
Alin Chiosa <alin.c.wiredelta@gmail.com>
2022-07-11 17:05:56 +0200

    chore: created routes to create, edit, delete storage item

--- src/routes/storage.ts
+++ src/routes/storage.ts
@@ -20,4 +20,53 @@ router.get('/by-category', isAuthenticated, function (req, res, next) {
     })
 })
 
+router.put('/:itemId', isAuthenticated, function (req, res, next) {
+    const { itemId } = req.params
+    const { user } = req.body
+
+    const { name, category, measurementUnit, quantity } = req.body.item
+
+    if (!user || user.role !== 'admin') {
+        return next(new Error('You dont have the rights'))
+    }
+
+    Storage.findByIdAndUpdate(itemId, {
+        name,
+        category,
+        measurementUnit,
+        quantity,
+    }).then((newItem) => res.json({ item: newItem }))
+})
+
+router.delete('/:itemId', isAuthenticated, function (req, res, next) {
+    const { itemId } = req.params
+    const { user } = req.body
+
+    if (!user || user.role !== 'admin') {
+        return next(new Error('You dont have the rights'))
+    }
+
+    Storage.findByIdAndDelete(itemId).then(() =>
+        res.json({ message: 'Item deletet successfully' })
+    )
+})
+
+router.post('/', isAuthenticated, function (req, res, next) {
+    const { user } = req.body
+    const { name, category, measurementUnit, quantity } = req.body.item
+
+    if (!user || user.role !== 'admin') {
+        return next(new Error('You dont have the rights'))
+    }
+
+    if (!name || !category || !measurementUnit || !quantity) {
+        return next(new Error('You need more stuff'))
+    }
+
+    Storage.create({ name, measurementUnit, quantity, category }).then(
+        (newItem) =>
+            res.json({ message: 'Item created successfully', item: newItem })
+    )
+})
+
 export default router
