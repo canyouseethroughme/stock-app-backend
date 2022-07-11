3a09478ee43775bf6bafacb4c134b3c1ff33d4a5
Alin Chiosa <alin.c.wiredelta@gmail.com>
2022-07-11 16:15:38 +0200

    chore: send items by category

--- src/routes/storage.ts
+++ src/routes/storage.ts
@@ -8,4 +8,16 @@ router.get('/', isAuthenticated, function (req, res, next) {
     Storage.find({}).then((items) => res.json({ items }))
 })
 
+router.get('/by-category', isAuthenticated, function (req, res, next) {
+    Storage.find({}).then((items) => {
+        const categories = new Set(items.map((el) => el.category))
+        const filteredItems = [...categories].map((item) => ({
+            category: item,
+            brands: items.filter((el) => el.category === item),
+        }))
+
+        res.json({ items: filteredItems })
+    })
+})
+
 export default router
