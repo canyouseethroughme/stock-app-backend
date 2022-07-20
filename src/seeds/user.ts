6d29fe6677e154385b1e6dab0b85f75941aeb003
Alin Chiosa <alin.c.wiredelta@gmail.com>
2022-07-20 10:14:42 +0200

    chore: updated seeds

--- src/seeds/user.ts
+++ src/seeds/user.ts
@@ -3,96 +3,144 @@ import { User } from '../models/user'
 export const createUsers = () => {
     User.create({
         role: 'admin',
-        username: 'admin',
-        password: 'admin',
+        username: 'aliecs',
+        password: 'ialapula',
     })
 
     User.create({
         role: 'delivery',
-        username: 'delivery1',
-        password: 'delivery1',
+        username: 'cart1',
+        password: 'tools',
     })
     User.create({
         role: 'delivery',
-        username: 'delivery2',
-        password: 'delivery2',
+        username: 'cart2',
+        password: 'brave',
     })
     User.create({
         role: 'delivery',
-        username: 'delivery3',
-        password: 'delivery3',
+        username: 'cart3',
+        password: 'look',
     })
     User.create({
         role: 'delivery',
-        username: 'delivery4',
-        password: 'delivery4',
+        username: 'cart4',
+        password: 'give',
     })
     User.create({
         role: 'delivery',
-        username: 'delivery5',
-        password: 'delivery5',
+        username: 'cart5',
+        password: 'find',
     })
     User.create({
         role: 'delivery',
-        username: 'delivery6',
-        password: 'delivery6',
-    })
-    User.create({
-        role: 'delivery',
-        username: 'delivery7',
-        password: 'delivery7',
+        username: 'cart6',
+        password: 'year',
     })
 
     User.create({
         role: 'bar',
-        username: 'bar1',
-        password: 'bar1',
+        username: 'alejandro',
+        password: 'altru',
         barName: 'Vessel',
     })
     User.create({
         role: 'bar',
-        username: 'bar2',
-        password: 'bar2',
+        username: 'anna',
+        password: 'aneva',
         barName: 'Space',
     })
     User.create({
         role: 'bar',
-        username: 'bar3',
-        password: 'bar3',
+        username: 'daniel',
+        password: 'danea',
         barName: 'Garden',
     })
     User.create({
         role: 'bar',
-        username: 'bar4',
-        password: 'bar4',
-        barName: 'Vessel',
-    })
-    User.create({
-        role: 'bar',
-        username: 'bar5',
-        password: 'bar5',
+        username: 'hugo',
+        password: 'hubro',
         barName: 'Space',
     })
     User.create({
         role: 'bar',
-        username: 'bar6',
-        password: 'bar6',
+        username: 'mikael',
+        password: 'mifry',
+        barName: 'Vessel',
+    })
+    User.create({
+        role: 'bar',
+        username: 'simon',
+        password: 'silah',
+        barName: 'Space',
+    })
+    User.create({
+        role: 'bar',
+        username: 'niels',
+        password: 'niped',
+        barName: 'Space',
+    })
+    User.create({
+        role: 'bar',
+        username: 'aline',
+        password: 'alwan',
         barName: 'Garden',
     })
     User.create({
         role: 'bar',
-        username: 'bar7',
-        password: 'bar7',
+        username: 'saska',
+        password: 'aloro',
+        barName: 'Garden',
+    })
+    User.create({
+        role: 'bar',
+        username: 'emma',
+        password: 'emnie',
         barName: 'Vessel',
     })
+    User.create({
+        role: 'bar',
+        username: 'danilo',
+        password: 'datim',
+        barName: 'Space',
+    })
+    User.create({
+        role: 'bar',
+        username: 'allou',
+        password: 'allou1',
+        barName: 'Vessel',
+    })
+    User.create({
+        role: 'bar',
+        username: 'vadims',
+        password: 'vapit',
+        barName: 'Space',
+    })
+    User.create({
+        role: 'bar',
+        username: 'liva',
+        password: 'lilie',
+        barName: 'Garden',
+    })
+    User.create({
+        role: 'bar',
+        username: 'andreas',
+        password: 'anjor',
+        barName: 'Garden',
+    })
+    User.create({
+        role: 'bar',
+        username: 'casper',
+        password: 'cahol',
+        barName: 'Garden',
+    })
 
-    User.create({ role: 'storage', username: 'storage1', password: 'storage1' })
-    User.create({ role: 'storage', username: 'storage2', password: 'storage2' })
-    User.create({ role: 'storage', username: 'storage3', password: 'storage3' })
-    User.create({ role: 'storage', username: 'storage4', password: 'storage4' })
-    User.create({ role: 'storage', username: 'storage5', password: 'storage5' })
-    User.create({ role: 'storage', username: 'storage6', password: 'storage6' })
-    User.create({ role: 'storage', username: 'storage7', password: 'storage7' })
+    User.create({ role: 'storage', username: 'store1', password: 'tools' })
+    User.create({ role: 'storage', username: 'store2', password: 'brave' })
+    User.create({ role: 'storage', username: 'store3', password: 'look' })
+    User.create({ role: 'storage', username: 'store4', password: 'give' })
+    User.create({ role: 'storage', username: 'store5', password: 'find' })
+    User.create({ role: 'storage', username: 'store6', password: 'year' })
 
     console.log('saved')
 }
