1cd171d213010ba889725f615aa54bdf0ef498b7
alin-c-wiredelta <alin.c.wiredelta@gmail.com>
2023-07-16 17:17:30 +0200

    chore: added get user functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -46,4 +46,40 @@ router.get('/test-token', isAuthenticated, function (req, res, next) {
     res.send({ muie: res.req.body.user })
 })
 
+// create user
+router.post('/create', function (req, res, next) {
+    const { role, username, barName, password } = req.body
+    if (!role || !username || !barName || !password) {
+        return next(new Error('Wrong input'))
+    }
+
+    User.findOne({ username })
+        .exec()
+        .then((user) => {
+            if (user) return next(new Error('Username already exists'))
+
+            User.create({ role, username, barName, password }).then((newUser) =>
+                res.json({ message: 'User created successfully', use: newUser })
+            )
+        })
+})
+
+// getUsersByCategory
+router.get('/getUsersByCategory', isAuthenticated, function (req, res, next) {
+    const { user } = req.body
+
+    const { role } = req.query
+
+    if (user?.role !== 'admin') {
+        return next(new Error("You don't have the rights"))
+    }
+
+    return User.find({ role: role }).then((items) => {
+        console.log('🚀 ~ file: user.ts:79 ~ returnUser.find ~ items:', items)
+        return res.json({
+            users: items,
+        })
+    })
+})
+
 export default router
